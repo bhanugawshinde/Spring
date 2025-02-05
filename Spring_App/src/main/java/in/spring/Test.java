488ec1c361d6abffc1c2dc31ad6d3047a61915b0
Bhanu Gawshinde <bhanugawshinde@gmail.com>
2025-02-05 22:58:21 +0530

    spring project intialized.

---
+++ Spring_App/src/main/java/in/spring/Test.java
@@ -0,0 +1,12 @@
+package in.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	public static void main(String[] args) {
+		ApplicationContext context = 
+        		new ClassPathXmlApplicationContext("spring-bean.xml");
+		System.out.print(context);
+	}
+}
